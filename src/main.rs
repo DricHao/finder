08406534165a1a02fd0c4fdc9a9d865415ce8a16
HAO <473164786@qq.com>
2018-07-18 20:46:06 +0800

    start

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
 
-trait Console_name {
-    fn console(&self) ->String {
-        "naruto!".to_string()
-    }
-    fn format_name(&self) -> String;
-}
-#[derive(Debug)]
-struct Persion {
-    name: String,
-    age: u32,
-    addr: String,
-}
-impl Console_name for Persion {
-    fn format_name(&self) -> String {
-        let a = format!("{},{}",self.name,self.addr);
-        a
-    }
-}
-
+use std::env;
 
 fn main() {
-    let y = Persion {
-        name: String::from("joe michael"),
-        age: 22,
-        addr: String::from("beijing,china")
-    };
-    println!("{}", y.format_name());
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("在{}", filename);
+    println!("寻找{}中...", query);
+
 }
