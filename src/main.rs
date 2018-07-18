d3d11e0fdf0eaaf725d999e07bf4cea0fd8d638c
HAO <473164786@qq.com>
2018-07-18 20:57:11 +0800

    find file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
 
-    println!("在{}", filename);
-    println!("寻找{}中...", query);
+    println!("在{};", filename);
+    println!("寻找'{}'中...", query);
+
+    let mut f = File::open(filename).expect("该文件没有找到");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("wrong!");
+
+    println!("文件内容是： {}", contents);
 
 }
